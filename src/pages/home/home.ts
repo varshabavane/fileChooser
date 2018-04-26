e8b1efa33281bbab5103084613e77d517671de1e
varsha bavane <varshab13bavane@gmail.com>
2018-04-26 11:42:45 +0530

    funcionality added to get  details of songs

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -31,33 +31,9 @@ export class HomePage {
             ) {
               this.getFileList(path + "/" + item.name);
             } else {
-               alert("fileName insde filelist " + JSON.stringify(item.fullPath));
+              alert("fileName insde filelist " + JSON.stringify(item.fullPath));
               // this.file
-              try {
-                let filetype = new File();
-                filetype
-                  .resolveLocalFilesystemUrl(
-                    file.externalRootDirectory + item.fullPath
-                  )
-                  .then((songDetail: FileEntry) => {
-                    alert("songDetail: "+ JSON.stringify(songDetail));
-                    console.log("songdetail: ", JSON.stringify(songDetail));
-                    return new Promise((resolve, reject) => {
-                      songDetail.file(
-                        sDetail => resolve(sDetail),
-                        error => reject(error)
-                      );
-                    }).then((sDetail: IFile) => {
-                      alert("Detail: " + JSON.stringify(sDetail));
-                    });
-                  });
-              } catch {
-                e => alert(JSON.stringify(e));
-              }
-              this._fileList.push({
-                name: item.name,
-                path: item.fullPath
-              });
+              this.fileType(item);
             }
           }
         },
@@ -90,10 +66,11 @@ export class HomePage {
           } else if (item.isFile == true) {
             //File found
             //alert("fileName inside fileSearch" + JSON.stringify(item));
-            this._fileList.push({
-              name: item.name,
-              path: item.fullPath
-            });
+            // this._fileList.push({
+            //   name: item.name,
+            //   path: item.fullPath
+            // });
+            this.fileType(item);
           }
         }
       })
@@ -103,10 +80,38 @@ export class HomePage {
   }
 
   FileDir() {
-    alert("filelist:" + JSON.parse(this._fileList[0]));
+    //alert("filelist:" + JSON.parse(this._fileList[0]));
   }
-fileType(songDetail){
 
-}
- 
+  fileType(songDetail) {
+    alert("songPath " + JSON.stringify(songDetail));
+    try {
+      let filetype = new File();
+      filetype
+        .resolveLocalFilesystemUrl(
+          this.file.externalRootDirectory + songDetail.fullPath
+        )
+        .then((songDetail: FileEntry) => {
+         // alert("songDetail: " + JSON.stringify(songDetail));
+          console.log("songdetail: ", JSON.stringify(songDetail));
+          return new Promise((resolve, reject) => {
+            songDetail.file(
+              sDetail => resolve(sDetail),
+              error => reject(error)
+            );
+          }).then((sDetail: IFile) => {
+            //alert("Detail: " + JSON.stringify(sDetail));
+             if(sDetail.type=='audio/mpeg'){
+               alert( "mp3songs " +JSON.stringify(sDetail))
+            this._fileList.push({
+              name: songDetail.name,
+              path: songDetail.fullPath
+            });
+             }
+          });
+        });
+    } catch {
+      e => alert(JSON.stringify(e));
+    }
+  }
 }
